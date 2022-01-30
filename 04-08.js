9c311853647ae9cff6f1166b82c751176b39a258
cdspacenoob <cdspacenoob@gmail.com>
2022-01-31 08:36:10 +0900

    타입 변환

---
+++ 04-08.js
@@ -0,0 +1,6 @@
+console.log(a);
+
+a = 80;
+var a;
+
+console.log(a);
---
+++ 06-13.js
@@ -0,0 +1,11 @@
+var a = 65;
+console.log(a);
+
+var b = '65';
+console.log(b);
+
+var c = 'A';
+console.log(c);
+
+var d = A;
+console.log(d);
